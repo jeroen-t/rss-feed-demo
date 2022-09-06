af1b0ac8154f404f1901ef9aa35986a1a69c6abf
Jeroen Trimbach <jeroen.trimbach@gmail.com>
2022-09-06 22:29:04 +0200

    boostrap 4 card deck

--- test.go
+++ test.go
@@ -10,6 +10,6 @@ func main() {
 	fp := gofeed.NewParser()
 	feed, _ := fp.ParseURL("https://azurecomcdn.azureedge.net/en-us/updates/feed/")
 	for i := 0; i < len(feed.Items); i++ {
-		fmt.Println(feed.Items[i].Title)
+		fmt.Printf("%s %s\n", feed.Items[i].Title, feed.Items[i].Description)
 	}
 }
